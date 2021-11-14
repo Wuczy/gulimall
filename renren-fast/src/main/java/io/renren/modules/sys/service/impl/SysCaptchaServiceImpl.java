bdd05b8f476ba0d1e077a74db01699dcd52a8c16
wczy <wuchengzhi@hydee.cn>
2021-11-15 00:22:30 +0800

    heat:后台管理搭建使用“人人开源”&验证码走redis

---
+++ renren-fast/src/main/java/io/renren/modules/sys/service/impl/SysCaptchaServiceImpl.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ * <p>
+ * https://www.renren.io
+ * <p>
+ * 版权所有，侵权必究！
+ */
+
+package io.renren.modules.sys.service.impl;
+
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.code.kaptcha.Producer;
+import io.renren.common.exception.RRException;
+import io.renren.common.utils.RedisUtils;
+import io.renren.modules.sys.dao.SysCaptchaDao;
+import io.renren.modules.sys.entity.SysCaptchaEntity;
+import io.renren.modules.sys.service.SysCaptchaService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * 验证码
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Service("sysCaptchaService")
+public class SysCaptchaServiceImpl extends ServiceImpl<SysCaptchaDao, SysCaptchaEntity> implements SysCaptchaService {
+    @Autowired
+    private Producer producer;
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @Override
+    public BufferedImage getCaptcha(String uuid) {
+        if (StringUtils.isBlank(uuid)) {
+            throw new RRException("uuid不能为空");
+        }
+        //生成文字验证码
+        String code = producer.createText();
+
+        SysCaptchaEntity captchaEntity = new SysCaptchaEntity();
+        captchaEntity.setUuid(uuid);
+        captchaEntity.setCode(code);
+        //5分钟后过期
+        redisUtils.set(uuid, code, 300);
+
+//        captchaEntity.setExpireTime(DateUtils.addDateMinutes(new Date(), 5));
+//        this.save(captchaEntity);
+
+        return producer.createImage(code);
+    }
+
+    @Override
+    public boolean validate(String uuid, String code) {
+        String entityCode = redisUtils.get(uuid);
+        if (StrUtil.isBlank(entityCode)) {
+            return false;
+        } else if (StrUtil.equals(entityCode, code)) {
+            //删除验证码
+            redisUtils.delete(uuid);
+            return true;
+        }
+        return false;
+
+
+//        SysCaptchaEntity captchaEntity = this.getOne(new QueryWrapper<SysCaptchaEntity>().eq("uuid", uuid));
+//        //
+//        this.removeById(uuid);
+//
+//        if(captchaEntity.getCode().equalsIgnoreCase(code) && captchaEntity.getExpireTime().getTime() >= System.currentTimeMillis()){
+//            return true;
+//        }
+//        return false;
+    }
+}
