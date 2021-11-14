45574705c76e92872b2ffef7420bc56e0916f388
wczy <wuchengzhi@hydee.cn>
2021-11-14 19:38:40 +0800

    heat:项目maven搭建

---
+++ gulimall-product/src/test/java/com/wczy/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wczy.gulimall.product;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
